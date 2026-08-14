5e8253054d509782557f3b9c3cb531dbecb4a046
Seenu Seenu <seenu1432123@gmail.com>
2026-08-14 18:23:57 +0530

    Addad some images

--- src/data/products.js
+++ src/data/products.js
@@ -1,7 +1,18 @@
 import bottle from "../assets/images/product-bottle.jpeg";
 import fieldBottle from "../assets/images/field-bottle.jpeg";
-
+import curd1 from "../assets/images/Curd1L.png";
+import curd2 from "../assets/images/Curd2L.png";
+import curd from "../assets/images/Curd500ML.png";
 export const products = [
+   {
+    id: "raw-cow-0l",
+    name: "Fresh Raw Cow Milk",
+    unit: "2 Litres",
+    price: 160,
+    image: bottle,
+    badge: "Family Pack",
+    description: "A practical family-size quantity for regular delivery."
+  },
   {
     id: "raw-cow-1l",
     name: "Fresh Raw Cow Milk",
@@ -10,15 +21,6 @@ export const products = [
     image: bottle,
     badge: "Best Seller",
     description: "Fresh milk for your family's everyday routine."
-  },
-   {
-    id: "fresh-curd-01",
-    name: "Fresh Curd",
-    unit: "1 Litres",
-    price: 100,
-    image: bottle,
-    badge: "Family Pack",
-    description: "A practical family-size quantity for regular delivery."
   },
   {
     id: "raw-cow-500",
@@ -32,20 +34,29 @@ export const products = [
     {
     id: "fresh-curd-02",
     name: "Fresh Curd",
-    unit: "1 Litres",
+    unit: "2 Litres",
     price: 100,
-    image: bottle,
+    image: curd2,
     badge: "Family Pack",
     description: "A practical family-size quantity for regular delivery."
+  },
+      {
+    id: "fresh-curd-01",
+    name: "Fresh Curd",
+    unit: "1 Litres",
+    price: 100,
+    image: curd1,
+    badge: "Best Seller",
+    description: "Fresh milk for your family's everyday routine."
   },
     {
     id: "fresh-curd-03",
     name: "Fresh Curd",
-    unit: "1 Litres",
+    unit: "500 ml",
     price: 100,
-    image: bottle,
-    badge: "Family Pack",
-    description: "A practical family-size quantity for regular delivery."
+    image: curd,
+    badge: "Popular",
+    description: "A convenient half-litre option for smaller households."
   },
     {
     id: "fresh-curd-04",
@@ -70,15 +81,6 @@ export const products = [
     name: "Fresh Curd",
     unit: "1 Litres",
     price: 100,
-    image: bottle,
-    badge: "Family Pack",
-    description: "A practical family-size quantity for regular delivery."
-  },
-  {
-    id: "raw-cow-0l",
-    name: "Fresh Raw Cow Milk",
-    unit: "2 Litres",
-    price: 160,
     image: bottle,
     badge: "Family Pack",
     description: "A practical family-size quantity for regular delivery."
